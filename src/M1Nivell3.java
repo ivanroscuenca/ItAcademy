b5bc026062d3e259a504f74e2a3c3764aa667fdf
root <root@ivan-Lenovo-B590>
2021-07-02 14:39:36 +0200

    M1 nivell 3 Upload

---
+++ src/M1Nivell3.java
@@ -0,0 +1,24 @@
+
+public class M1Nivell3 {
+
+	public static void main(String[] args) {
+		// M1 NIVELL 3 
+		
+		//Declara un array de numeros "int" e inicialitzala amb valors del 1 al 10
+		
+		int array1 [] = new int[10];
+			for (int i = 1; i <= array1.length; i++){
+		    System.out.print(i + " ");
+		}
+		System.out.print("\n");
+		 //Rota l’array sense utilitzar un array auxiliar ni llibreries. Pots utilizar una variable auxiliar.
+	
+		int array2 [] = new int[10];
+			for (int j = 10; j >=(array2.length)-10; j--){
+				    System.out.print(j + " ");
+				}
+		}
+		    
+	}
+
+
